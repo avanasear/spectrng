e43fd7bc7b7c8923cb13be6aeb0a3df370e83d82
Zach Kaylor <avanasear@gmail.com>
2020-10-26 21:21:30 -0400

    added more runs

--- src/poc2.c
+++ src/poc2.c
@@ -1,30 +1,101 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <unistd.h>
-#include <signal.h>
-#include <pthread.h>
 #include <immintrin.h>
 
-/*
- This is essentially a proof of concept for the proof of concept
- We're interested to see the shortest amount of time it takes to have it read and not fail
-*/
-int main(int argc, char ** argv)
+int main(void)
 {
-	int x = 0;
-	unsigned long long p;
-	int arr[1] = {0};
+    int i, j = 0;
+    int ret;
 
-	for (x = 0; x < 1; x++)
-	{
-		arr[x] = _rdseed64_step(&p);
-		usleep(100);
-	}
-
-	for (x=0; x < 1; x++) {
-		printf("%d", arr[x]);
-	}
-	return 0;
+    for (i = 0; i < 99; i++) {
+        j = 0;
+        asm volatile(
+            "rdseed %%rax;"
+            "rdseed %%rax;"
+            "rdseed %%rax;"
+            "rdseed %%rax;"
+            "rdseed %%rax;"
+            "rdseed %%rax;" : "=r" (ret)
+        );
+        if (ret == 0) {
+            j++;
+            printf("fail %d\n", j);
+        }
+        sleep(2);
+    }
+    for (i = 0; i < 99; i++) {
+        j = 0;
+        asm volatile(
+            "rdseed %%rax;"
+            "rdseed %%rax;"
+            "rdseed %%rax;"
+            "rdseed %%rax;"
+            "rdseed %%rax;"
+            "rdseed %%rax;"
+            "rdseed %%rax;" : "=r" (ret)
+        );
+        if (ret == 0) {
+            j++;
+            printf("fail %d\n", j);
+        }
+        sleep(2);
+    }
+    for (i = 0; i < 99; i++) {
+        j = 0;
+        asm volatile(
+            "rdseed %%rax;"
+            "rdseed %%rax;"
+            "rdseed %%rax;"
+            "rdseed %%rax;"
+            "rdseed %%rax;"
+            "rdseed %%rax;"
+            "rdseed %%rax;"
+            "rdseed %%rax;" : "=r" (ret)
+        );
+        if (ret == 0) {
+            j++;
+            printf("fail %d\n", j);
+        }
+        sleep(2);
+    }
+    for (i = 0; i < 99; i++) {
+        j = 0;
+        asm volatile(
+            "rdseed %%rax;"
+            "rdseed %%rax;"
+            "rdseed %%rax;"
+            "rdseed %%rax;"
+            "rdseed %%rax;"
+            "rdseed %%rax;"
+            "rdseed %%rax;"
+            "rdseed %%rax;"
+            "rdseed %%rax;" : "=r" (ret)
+        );
+        if (ret == 0) {
+            j++;
+            printf("fail %d\n", j);
+        }
+        sleep(2);
+    }
+    for (i = 0; i < 99; i++) {
+        j = 0;
+        asm volatile(
+            "rdseed %%rax;"
+            "rdseed %%rax;"
+            "rdseed %%rax;"
+            "rdseed %%rax;"
+            "rdseed %%rax;"
+            "rdseed %%rax;"
+            "rdseed %%rax;"
+            "rdseed %%rax;"
+            "rdseed %%rax;"
+            "rdseed %%rax;" : "=r" (ret)
+        );
+        if (ret == 0) {
+            j++;
+            printf("fail %d\n", j);
+        }
+        sleep(2);
+    }
 }
-
 
